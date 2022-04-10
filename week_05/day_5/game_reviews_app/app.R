1e7ba0eb4192421beac6860645347620ec30ec73
mrcslx <100318856+mrcslx@users.noreply.github.com>
2022-04-10 21:53:17 +0100

    Game reviews app finished and working

--- week_05/day_5/game_reviews_app/app.R
+++ week_05/day_5/game_reviews_app/app.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(tidyverse)
 library(DT)
+library(shinythemes)
 
 games <- CodeClanData::game_sales
 all_genres <- sort(unique(games$genre))
@@ -9,7 +10,9 @@ all_platforms <- sort(unique(games$platform))
 
 ui <- fluidPage(
   
-  titlePanel(h1("💩🕹 Turd Finder 3000 🕹💩")),
+  theme = shinytheme("united"),
+  
+  titlePanel(h1("Bad Game Finder 🤢🕹")),
   
   sidebarLayout(
     
@@ -18,11 +21,11 @@ ui <- fluidPage(
       selectInput("platform_input", label = h4("Platform"), 
                   choices = all_platforms),
       
-      checkboxGroupInput("genre_input", label = h4("Game genre"), 
+      checkboxGroupInput("genre_input", label = h4("Genre"), 
                          choices = all_genres,
                          selected = all_genres),
       
-      sliderInput("year_input", label = h4("Release year range"), min = 1988, 
+      sliderInput("year_input", label = h4("Released"), min = 1988, 
                   max = 2016, value = c(1988, 2016), sep = "")
       
     ),
@@ -32,10 +35,12 @@ ui <- fluidPage(
         tabPanel("Worst reviewed games", 
                  HTML("<br>"),
                  plotOutput("critic_ratings_plot"), 
-                 HTML("<br><br>"),
+                 HTML("<br>"),
                  plotOutput("user_ratings_plot")), 
-        tabPanel("Summary", verbatimTextOutput("summary")), 
-        tabPanel("Table view", tableOutput("table"))
+        tabPanel("Table view", dataTableOutput("games_table")),
+        tabPanel("Don't click", 
+                 HTML("<br><br>"),
+                      uiOutput("img"))
       )
     )
   )
@@ -46,11 +51,13 @@ server <- function(input, output) {
   filtered_games <- reactive({
     games %>% 
       filter(platform %in% input$platform_input) %>% 
-      select(-c(sales, rating, platform)) %>% 
+      select(-c(publisher, sales, rating, platform)) %>% 
       filter(genre %in% input$genre_input) %>%
       filter(year_of_release %in% seq(input$year_input[1], input$year_input[2], 
                                       by = 1))
   })
+  
+  # The following is a bar chart that shows the 10 lowest rated games (by professional critics) for a platform chosen by the user. I have limited it to a bottom 10 in order to keep it legible, however there is also a table view in the second tab should the user want more information. I have set up the dashboard to show one platform at a time, however the user can also select one or several game genres as well as a range of release years. The aim is to highlight the worst games in the dataset so that the user can avoid them or maybe intentionally seek them out if they are so inclined.
   
   output$critic_ratings_plot <- renderPlot({
     
@@ -64,12 +71,15 @@ server <- function(input, output) {
       labs(x = "Critic score",
            y = "Game title",
            title = "Worst games by critic score") +
-      theme(axis.text = element_text(size = 11),
+      theme_minimal() +
+      theme(title = element_text(size = 14),
+            axis.text = element_text(size = 11),
             axis.title.x = element_text(size = 14),
             axis.title.y = element_text(size = 0))
     
   })
   
+  # The following plot is similar to the first one, however instead of critic ratings it shows user ratings. It is a good supplement to the first plot as user ratings can sometimes be more dependable than professional ones.
   
   output$user_ratings_plot <- renderPlot({
     
@@ -83,10 +93,23 @@ server <- function(input, output) {
       labs(x = "User score",
            y = "Game title",
            title = "Worst games by user score") +
-      theme(axis.text = element_text(size = 11),
+      theme_minimal() +
+      theme(title = element_text(size = 14),
+            axis.text = element_text(size = 11),
             axis.title.x = element_text(size = 14),
             axis.title.y = element_text(size = 0))
     
+  })
+  
+  output$games_table <- renderDataTable({
+    
+    filtered_games() %>%
+      arrange(user_score)
+    
+  })
+  
+  output$img <- renderUI({
+    tags$img(src = "https://upload.wikimedia.org/wikipedia/en/9/9a/Trollface_non-free.png")
   })
   
 }
