d5169bebd4420cd5529bb51ee758dc1e5432de42
mrcslx <100318856+mrcslx@users.noreply.github.com>
2022-04-10 15:53:53 +0100

    Both plots working

--- week_05/day_5/game_reviews_app/app.R
+++ week_05/day_5/game_reviews_app/app.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(tidyverse)
+library(DT)
 
 games <- CodeClanData::game_sales
 all_genres <- sort(unique(games$genre))
@@ -8,28 +9,31 @@ all_platforms <- sort(unique(games$platform))
 
 ui <- fluidPage(
   
-  titlePanel("Game Reviews"),
+  titlePanel(h1("💩🕹 Turd Finder 3000 🕹💩")),
   
   sidebarLayout(
     
     sidebarPanel(
       
+      selectInput("platform_input", label = h4("Platform"), 
+                  choices = all_platforms),
+      
       checkboxGroupInput("genre_input", label = h4("Game genre"), 
                          choices = all_genres,
-                         selected = 1),
+                         selected = all_genres),
       
       sliderInput("year_input", label = h4("Release year range"), min = 1988, 
-                  max = 2016, value = c(1988, 2016), sep = ""),
-      
-      checkboxGroupInput("platform_input", label = h4("Platform"), 
-                         choices = all_platforms,
-                         selected = 1)
+                  max = 2016, value = c(1988, 2016), sep = "")
       
     ),
     
     mainPanel(
       tabsetPanel(
-        tabPanel("Highest reviewed games", plotOutput("critic_ratings_plot")), 
+        tabPanel("Worst reviewed games", 
+                 HTML("<br>"),
+                 plotOutput("critic_ratings_plot"), 
+                 HTML("<br><br>"),
+                 plotOutput("user_ratings_plot")), 
         tabPanel("Summary", verbatimTextOutput("summary")), 
         tabPanel("Table view", tableOutput("table"))
       )
@@ -42,17 +46,45 @@ server <- function(input, output) {
   output$critic_ratings_plot <- renderPlot({
     
     games %>% 
-      filter(genre == "genre_input", year_of_release == "year_input", 
-             platform == "platform_input") %>% 
+      filter(genre %in% input$genre_input, 
+             year_of_release %in% seq(input$year_input[1], input$year_input[2], by = 1), 
+             platform == input$platform_input) %>% 
       select(name, critic_score) %>% 
       arrange(desc(critic_score)) %>% 
-      head(10) %>% 
+      tail(10) %>% 
       ggplot() +
       aes(x = critic_score, y = reorder(name, critic_score)) +
-      geom_col(fill = "aquamarine") +
-      labs(x = "\nCritic score",
+      geom_col(fill = "burlywood3") +
+      geom_text(aes(label = critic_score), hjust = 1.5) +
+      labs(x = "Critic score",
            y = "Game title",
-           title = "Top games by critic score")
+           title = "Worst games by critic score") +
+      theme(axis.text = element_text(size = 11),
+            axis.title.x = element_text(size = 14),
+            axis.title.y = element_text(size = 0))
+    
+  })
+  
+  
+  output$user_ratings_plot <- renderPlot({
+    
+    games %>% 
+      filter(genre %in% input$genre_input, 
+             year_of_release %in% seq(input$year_input[1], input$year_input[2], by = 1), 
+             platform == input$platform_input) %>% 
+      select(name, user_score) %>% 
+      arrange(desc(user_score)) %>% 
+      tail(10) %>% 
+      ggplot() +
+      aes(x = user_score, y = reorder(name, user_score)) +
+      geom_col(fill = "lightgoldenrod3") +
+      geom_text(aes(label = user_score), hjust = 1.5) +
+      labs(x = "User score",
+           y = "Game title",
+           title = "Worst games by user score") +
+      theme(axis.text = element_text(size = 11),
+            axis.title.x = element_text(size = 14),
+            axis.title.y = element_text(size = 0))
     
   })
   
